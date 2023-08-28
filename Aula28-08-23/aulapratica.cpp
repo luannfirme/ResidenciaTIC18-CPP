efe72e37c5e79b57d968c6250952a70046642caa
luannfirme <luannfirme@gmail.com>
2023-08-28 16:44:31 -0300

    update

---
+++ Aula28-08-23/aulapratica.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    // float pi = 3 + 0.1415;
+
+    // float pi = 3 + 0.99;
+    // int intPi = pi;
+    // float floatPi = intPi;
+
+    char letra = 'a';
+    int intLetra = letra;
+
+    cout << intLetra << endl;
+}
